8635059146a17e1f466eaeec5d97b1c8b0fb09a9
Pavel Tkachenko <paveltkachenko.my@gmail.com>
2023-03-30 00:16:38 +0300

    Правки по производительности

--- src/utils/replace.ts
+++ src/utils/replace.ts
@@ -8,14 +8,16 @@ const ReplaceUtil = async(): Promise<void> => {
 		const files = await readDirForUtil();
 		
 		if (files.length) {
-			const serach = configUtil.serach as Array<string>;
+			const search = configUtil.serach as Array<string>;
 			const replaceed = configUtil.replaced as string;
 
 			for (const file of files) {
 				let dataText = await readFileForUtil(file);
-				const rgx = new RegExp(serach.map((el) => `${el}\\b`).join('|'), "g");
-				dataText = dataText.replaceAll(rgx, replaceed);
-				await writeFileForUtil(file, dataText);
+				const rgx = new RegExp(search.map((el) => `${el}\\b`).join('|'), "g");
+				if (dataText.match(rgx) !== null) {
+					const newDataText = dataText.replaceAll(rgx, replaceed);
+					await writeFileForUtil(file, newDataText);	
+				}
 			}
 		}
 	}
