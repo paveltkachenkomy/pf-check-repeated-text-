cc029d84f94d002aad39df23aa4dec9936d688ba
Павел Ткаченко <paveltkachenko.my@gmail.com>
2023-04-10 17:41:28 +0300

    доделал систему поиска, теперь не выводит не используемые текстовки

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,11 +1,11 @@
-import configUtil from "../config/config";
+import configUtil, { ProcessUtil } from "../config/config";
 import readDirForUtil from "../libs/readDir";
 import readFileForUtil from "../libs/readFile";
 
 const SearchUtil = async (): Promise<Map<string, number>> => {
 	try {
 		const files = await readDirForUtil();
-		const matchText:Map<string, number> = new Map();
+		const matchText: Map<string, number> = new Map();
 
 		if (files.length) {
 			const search = configUtil.serach as Array<string>;
@@ -21,6 +21,14 @@ const SearchUtil = async (): Promise<Map<string, number>> => {
 					const rgx = new RegExp(`${searchText}\\b`, "g");
 					matchText.set(searchText, curentLength + (dataText.match(rgx) || []).length);
 				}
+			}
+
+			if (configUtil.process == ProcessUtil.search) {
+				matchText.forEach((el, i) => {
+					if (!el) {
+						matchText.delete(i)
+					}
+				})
 			}
 		}
 		console.log(matchText);
